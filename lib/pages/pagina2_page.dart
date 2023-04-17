6ad3cb4db6c66b35afc01f8184d2b69c6a7b5162
Pedro Molina Espinosa <pedromolesp@gmail.com>
2023-04-17 21:24:46 +0200

    terminada 128

--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:programacion/models/usuario.dart';
+import 'package:programacion/pages/services/usuario_service.dart';
 
 class Pagina2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Página 1"),
+        title: const Text("Página 2"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             MaterialButton(
-              onPressed: () {},
+              onPressed: () {
+                usuarioService.cargarUsuario(Usuario(
+                    nombre: 'Pedro Molina',
+                    edad: 32,
+                    profesiones: ['Programador', 'Cabrero']));
+              },
               color: Colors.blue,
               child: const Text(
                 'Establecer usuario',
@@ -20,7 +27,9 @@ class Pagina2Page extends StatelessWidget {
               ),
             ),
             MaterialButton(
-              onPressed: () {},
+              onPressed: () {
+                usuarioService.cambiarEdad(30);
+              },
               color: Colors.blue,
               child: const Text(
                 'Cambiar edad',
