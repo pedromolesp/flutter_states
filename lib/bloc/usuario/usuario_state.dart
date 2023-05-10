cff081fb5b24b618198180c0e1d8516547b41feb
Pedro Molina Espinosa <pedromolesp@gmail.com>
2023-05-10 23:44:06 +0200

    tERMINADA

---
+++ lib/bloc/usuario/usuario_state.dart
@@ -0,0 +1,13 @@
+part of 'usuario_cubit.dart';
+
+@immutable
+abstract class UsuarioState {}
+
+class UsuarioInitial extends UsuarioState {
+  final existeUsuario = false;
+
+  @override
+  String toString() {
+    return 'Usuario existe: $existeUsuario';
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:programacion/bloc/usuario/usuario_cubit.dart';
 import 'package:programacion/pages/pagina1_page.dart';
 import 'package:programacion/pages/pagina2_page.dart';
 
@@ -9,14 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'pagina1',
-      routes: {
-        'pagina1': (_) => const Pagina1Page(),
-        'pagina2': (_) => Pagina2Page(),
-      },
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => UsuarioCubit())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'pagina1',
+        routes: {
+          'pagina1': (_) => const Pagina1Page(),
+          'pagina2': (_) => Pagina2Page(),
+        },
+      ),
     );
   }
 }
